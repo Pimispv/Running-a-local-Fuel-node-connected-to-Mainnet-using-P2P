38663e08560e2723ef9c75a87302ddb1873f495b
0xHawre <0xharway@gmail.com>
2024-10-18 13:09:11 +0330

    add

--- script.sh
+++ script.sh
@@ -1,3 +1,15 @@
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+~/Notes/Running-a-local-Fuel-node-connected-to-Mainnet-using-P2P (main ✔) git pull
+remote: Enumerating objects: 5, done.
+remote: Counting objects: 100% (5/5), done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 3 (delta 2), reused 0 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (3/3), 944 bytes | 188.00 KiB/s, done.
+From https://github.com/0xHawre/Running-a-local-Fuel-node-connected-to-Mainnet-using-P2P
+   3f43289..78d5485  main       -> origin/main
+Auto-merging script.sh
+Merge made by the 'ort' strategy.
+~/Notes/Running-a-local-Fuel-node-connected-to-Mainnet-using-P2P (main ✔) cat script.sh
 #!/bin/bash
 
 # Color variables for design
@@ -22,53 +34,40 @@ install_tmux() {
 }
 
 echo -e "${YELLOW}Installing Fuel...${RESET}"
-curl https://install.fuel.network | shell
+curl -s https://install.fuel.network | sh
 
-restart_shell() {
-    shell_name=$(basename "$SHELL")
-    if [ "$shell_name" = "zsh" ]; then
-        echo -e "${YELLOW}Restarting Zsh...${RESET}"
-        source "$HOME/.zshrc"
-    elif [ "$shell_name" = "bash" ]; then
-        echo -e "${YELLOW}Restarting Bash...${RESET}"
-        source "$HOME/.bashrc"
+get_peer_id() {
+    read -p "Do you have 'peer_id'? (yes/no): " has_peer_id
+    if [ "$has_peer_id" = "no" ]; then
+        echo -e "${YELLOW}Generating new peer id....${RESET}"
+        fuel-core-keygen new --key-type peering > temp.txt
+        sleep 2
+        peer_id=$(grep -o '16Uiu2HAm[^\"]*' temp.txt)
+        echo "{\"peer_id\":\"$peer_id\"}" > wallet.txt
+        echo -e "${GREEN}New peer_id saved in wallet.txt${RESET}"
+        rm temp.txt
+    elif [ "$has_peer_id" = "yes" ]; then
+        read -p "Please enter your peer_id: " secret
+        echo "{\"peer_id\":\"$secret\"}" > wallet.txt
+        echo -e "${GREEN}Provided peer_id saved in wallet.txt${RESET}"
     else
-        echo -e "${RED}Unknown shell: $shell_name${RESET}"
+        echo -e "${RED}Invalid input. Please type 'yes' or 'no'.${RESET}"
     fi
-
-
-    get_peer_id() {
-  read -p "Do you have 'peer_id'? (yes/no):  " has_peer_id 
-  if [ "$has_peer_id" = "no" ]; then 
-    echo -e "${YELLOW}Generating new peer id....${RESET}"
-    fuel-core-keygen new --key-type peering > temp.txt
-    sleep 2
-    peer_id=$(cat temp.txt | grep -o '16Uiu2HAm[^\"]*')
-    echo "{\"peer_id\":\"$peer_id\"}" > wallet.txt
-    echo -e "${GREEN}New peer_id saved in wallet.txt${RESET}"
-    rm temp.txt
-  elif [ "$has_peer_id" = "yes" ]; then 
-    read -p "Please enter your peer_id: " secret
-    echo "{\"peer_id\":\"$secret\"}" > wallet.txt
-    echo -e "${GREEN}Provided peer_id saved in wallet.txt${RESET}"
-  else
-    echo -e "${RED}Invalid input. Please type 'yes' or 'no'.${RESET}"
-  fi
 }
 
 get_peer_id
 
-sleep 5  
-peer_id=$(grep -o '"peer_id":"[^"]*' wallet.txt | sed 's/"peer_id":"//')
-echo "The peer_id is: $peer_id"
+sleep 5
+secret=$(grep -o '"secret":"[^"]*' wallet.txt | sed 's/"peer_id":"//')
+echo "The secret: $secret"
 
-sleep 5 
+sleep 5
 
+# Set open file limit
 ulimit -S -n 32768
 
-
-read -p "main-net node name:  " nodename 
-reade -p "ETH main-net RPC: " RPC 
+read -p "main-net node name: " nodename
+read -p "ETH main-net RPC: " RPC
 
 tmux new-session -d -s fuel-node
 
@@ -76,7 +75,7 @@ tmux send-keys -t fuel-node "
 fuel-core run \
 --enable-relayer \
 --service-name ${nodename} \
---keypair ${peer_id} \
+--keypair ${secret} \
 --relayer ${RPC} \
 --ip=0.0.0.0 --port 4000 --peering-port 30333 \
 --db-path ~/.fuel-mainnet \
@@ -87,6 +86,7 @@ fuel-core run \
 --relayer-v2-listening-contracts=0xAEB0c00D0125A8a788956ade4f4F12Ead9f65DDf \
 --relayer-da-deploy-height=20620434 \
 --relayer-log-page-size=100 \
---sync-block-stream-buffer-size 30" C-m 
+--sync-block-stream-buffer-size 30" C-m
 
-echo "check logs : tmux attach-session -t fuel-node "
+echo -e "${YELLOW}Check logs: tmux attach-session -t fuel-node${RESET}"
+
